e39d59ae727301f9f5c60175ba1642ae0d18a181
skambarov <saidaminqambarov408@gmail.com>
2022-06-15 19:00:01 +0300

    make it more optimal

--- src/App.js
+++ src/App.js
@@ -2,49 +2,29 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
-function App() {
-  let [one, setOne] = useState("");
-  let [two, setTwo] = useState("");
-  let [three, setThree] = useState("");
-  let [four, setFour] = useState("");
-  let [five, setFive] = useState("");
-  let [six, setSix] = useState("");
-  let oneFunc = () => {
-    setOne((one = ""));
-    setTwo((two = ""));
-    setThree((three = ""));
-    setFour((four = ""));
-    setFive((five = ""));
-    setSix((six = 0));
+let count = 0;
+let res = "";
+const App = () => {
+  const [timer, setTimer] = useState("");
+  const [isBegin, setIsBegin] = useState(false);
 
+  const begin = () => {
     setInterval(() => {
-      setOne((one = 1));
+      count++;
+      res += "  " + count;
+      setTimer(res);
+      console.log(count);
+      if (count === 5) {
+        count = 0;
+        res = "";
+      }
     }, 1000);
-
-    setInterval(() => {
-      setTwo((two = 2));
-    }, 2020);
-
-    setInterval(() => {
-      setThree((three = 3));
-    }, 2800);
-
-    setInterval(() => {
-      setFour((four = 4));
-    }, 4423);
-
-    setInterval(() => {
-      setFive((five = 5));
-    }, 5230);
-
-    setInterval(() => {
-      setSix((six = 6));
-    }, 6300);
   };
-  if (six === 6) {
-    oneFunc();
-  }
 
+  const reset = () => {
+    count = 0;
+    res = "";
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -52,16 +32,20 @@ function App() {
         <p>Hello React</p>
         <button
           style={{ padding: "6px 20px", fontSize: "18px" }}
-          onClick={oneFunc}
+          onClick={() => {
+            if (isBegin) reset();
+            else {
+              setIsBegin(true);
+              begin();
+            }
+          }}
         >
           Click
         </button>
-        <pre>
-          {one} {two} {three} {four} {five}
-        </pre>
+        <pre>{timer}</pre>
       </header>
     </div>
   );
-}
+};
 
 export default App;
