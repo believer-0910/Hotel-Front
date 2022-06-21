81b2527a6b72b5b5c4400eeb67a9ce22338507a2
skambarov <saidaminqambarov408@gmail.com>
2022-06-21 15:27:10 +0300

    woring with map and implement

---
+++ src/Model/Value.js
@@ -0,0 +1,9 @@
+export let map = new Map(
+        [
+            [1, [{"name": "Sam"}, {"name": "Tony"}, {"name": "Peter"}]],
+            [2, [{"name": "Nat"}, {"name": "Rodgers"}, {"name": "Bob"}]],
+            [3, [{"name": "Parker"}, {"name": "Natasha"}, {"name": "Ivan"}]],
+            [4, [{"name": "Joanna"}, {"name": "Bruse"}, {"name": "Jane"}]],
+            [5, [{"name": "Eduardo"}, {"name": "Clint"}, {"name": "Thor"}]]
+        ]
+    )
